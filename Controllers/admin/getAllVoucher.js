57ff96797e235135a18defa9ced8f456704217d1
shivam singh <ssultanpura9@gmail.com>
2024-05-17 00:03:20 +0530

    added modal and city and state

--- Controllers/admin/getAllVoucher.js
+++ Controllers/admin/getAllVoucher.js
@@ -2,10 +2,13 @@ const Vouchers = require("../../models/VoucherTable");
 const userTable = require("../../models/userTable");
 
 const getAllVoucher = async (req, res) => {
-  const response = await Vouchers.findAll({ include: [{ model: userTable,attributes:['firstName','lastName'] }] });
+  const response = await Vouchers.findAll({
+    include: [{ model: userTable, attributes: ["firstName", "lastName"] }],
+  });
   if (response.length == 0) {
     return res.status(200).json({ msg: "no voucher found" });
   }
+  console.log(response);
   return res.status(200).json({ userList: response });
 };
 module.exports = getAllVoucher;
--- util/database.js
+++ util/database.js
@@ -3,4 +3,5 @@ const db = new sequelize("TourVoucher", "root", "1234", {
   dialect: "mysql",
   host: "localhost",
 });
+
 module.exports=db
