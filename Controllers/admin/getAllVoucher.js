14adb9fc9fa8f389c8e6ff5f53f5a7c815b7f6f2
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-05-22 17:44:59 +0530

    bitrix authentication and authorization added based on user and admin

--- Controllers/admin/getAllVoucher.js
+++ Controllers/admin/getAllVoucher.js
@@ -2,11 +2,14 @@ const Vouchers = require("../../models/VoucherTable");
 const userTable = require("../../models/userTable");
 
 const getAllVoucher = async (req, res) => {
+  if (req.role != "Admin") {
+    return res.status(400).json({ msg: "You are not a admin" });
+  }
   const response = await Vouchers.findAll({
     include: [{ model: userTable, attributes: ["firstName", "lastName"] }],
   });
   if (response.length == 0) {
-    return res.status(200).json({ msg: "no voucher found" });
+    return res.status(400).json({ msg: "no voucher found" });
   }
   console.log(response);
   return res.status(200).json({ userList: response });
--- Routes/user.js
+++ Routes/user.js
@@ -4,11 +4,12 @@ const createTour = require("../Controllers/user/createTour");
 const getTour = require("../Controllers/user/getTour");
 const getTourExpenses = require("../Controllers/user/getTourExpenses");
 const addTourDetails = require("../Controllers/user/addTourDetails");
+const checkToken = require("../middleware/checkToken");
 
 const routes = express.Router();
-routes.post("/saveExpense", addExpense);
-routes.post("/createTour", createTour);
-routes.post("/getTour", getTour);
-routes.post("/getTourExpenses",getTourExpenses)
-routes.post("/addDetails",addTourDetails)
+routes.post("/saveExpense", checkToken,addExpense);
+routes.post("/createTour", checkToken, createTour);
+routes.post("/getTour", checkToken, getTour);
+routes.post("/getTourExpenses", checkToken,getTourExpenses);
+routes.post("/addDetails", checkToken,addTourDetails);
 module.exports = routes;
