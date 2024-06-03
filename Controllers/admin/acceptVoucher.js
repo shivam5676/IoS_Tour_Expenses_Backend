26cce2b7ef825c5bc68363822666842dc10be414
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-06-03 18:03:51 +0530

    updated logic for acceptance

--- Controllers/admin/acceptVoucher.js
+++ Controllers/admin/acceptVoucher.js
@@ -6,7 +6,8 @@ const dotenv = require("dotenv").config();
 
 const acceptVoucher = async (req, res) => {
   const voucherId = req.body.voucherId;
-  console.log(req.body.assignedTo, "..............=>");
+  // console.log(req.body);
+  // console.log(req.body.assignedTo, "..............=>");
   // return;
   // const accessToken = req.body.token;
   // const userId = req.body.userId;
@@ -65,7 +66,41 @@ const acceptVoucher = async (req, res) => {
           assignedTo: req.body.assignedTo,
         });
       }
+      async function sendApprovalRequest(currentUserid, nextUserId, voucherId) {
+        try {
+          // Send notification to the next user
+          await axios.post(
+            `https://${process.env.COMPANY_DOMAIN}/rest/tasks.task.add`,
+            null,
+            {
+              params: {
+                auth: req.body.token,
+                "fields[TITLE]": `Approval Request for Voucher ${voucherId}`,
+                "fields[DESCRIPTION]": `Please review and approve the Tour voucher with ID ${voucherId},Reject the Tour Voucher If You find any error in Expenses and thier bills`,
+                "fields[RESPONSIBLE_ID]": nextUserId,
+              },
+            }
+          );
 
+          // Send notification to the origin user
+          // await axios.post("https://your-bitrix24-url/rest/feed.item.add", {
+          //   auth: "your-access-token",
+          //   POST_TITLE: `Your Approval Request for Voucher ${voucherId}`,
+          //   POST_MESSAGE: `Your approval request for the voucher with ID ${voucherId} has been sent to ${nextUserId}`,
+          //   POST_SUB_TITLE: "Voucher Approval",
+          //   POST_FEED: "tasks",
+          //   DESTINATION: [{ TYPE: "USER", ID: currentUserid }],
+          // });
+
+          console.log("Approval request sent successfully");
+        } catch (error) {
+          // console.error("Error sending approval request:", error);
+          throw error;
+        }
+      }
+      if (req.body.assignedTo) {
+        sendApprovalRequest(req.body.userId, req.body.assignedTo, voucherId);
+      }
       console.log(updatedData);
       return res.status(200).json({ details: updatedData });
     }
