0eb42574167a941d9dcf1bff16971407d3d7e7d1
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-06-03 18:03:51 +0530

    updated logic for acceptance

--- Controllers/user/addTourDetails.js
+++ Controllers/user/addTourDetails.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const VouchersDescription = require("../../models/VoucherDescription");
 const Vouchers = require("../../models/VoucherTable");
 const assignedVoucher = require("../../models/assignedVoucher");
@@ -76,6 +77,42 @@ const addTourDetails = async (req, res) => {
       VoucherId: voucherId,
       userId: req.body.userId,
     });
+
+    async function sendApprovalRequest(currentUserid, nextUserId, voucherId) {
+      try {
+        // Send notification to the next user
+        await axios.post(
+          `https://${process.env.COMPANY_DOMAIN}/rest/tasks.task.add`,
+          null,
+          {
+            params: {
+              auth: req.body.token,
+              "fields[TITLE]": `Approval Request for Voucher ${voucherId}`,
+              "fields[DESCRIPTION]": `Please review and approve the Tour voucher with ID ${voucherId},Reject the Tour Voucher If You find any error in Expenses and thier bills`,
+              "fields[RESPONSIBLE_ID]": nextUserId,
+            },
+          }
+        );
+
+        // Send notification to the origin user
+        // await axios.post("https://your-bitrix24-url/rest/feed.item.add", {
+        //   auth: "your-access-token",
+        //   POST_TITLE: `Your Approval Request for Voucher ${voucherId}`,
+        //   POST_MESSAGE: `Your approval request for the voucher with ID ${voucherId} has been sent to ${nextUserId}`,
+        //   POST_SUB_TITLE: "Voucher Approval",
+        //   POST_FEED: "tasks",
+        //   DESTINATION: [{ TYPE: "USER", ID: currentUserid }],
+        // });
+
+        console.log("Approval request sent successfully");
+      } catch (error) {
+        // console.error("Error sending approval request:", error);
+        throw error;
+      }
+    }
+    if (req.body.assignedTo) {
+      sendApprovalRequest(req.body.userId, req.body.assignedTo, voucherId);
+    }
     console.log(updatedData);
     return res.status(200).json({ details: updatedData });
   } catch (err) {
