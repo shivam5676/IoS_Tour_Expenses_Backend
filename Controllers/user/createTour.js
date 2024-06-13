e21f84a5cf807328387bc986e70642fe0f85d6d9
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-06-13 18:00:44 +0530

    added exchange rates

--- Controllers/user/createTour.js
+++ Controllers/user/createTour.js
@@ -1,6 +1,31 @@
 const Vouchers = require("../../models/VoucherTable");
 // const assignedVoucher = require("../../models/assignedVoucher");
+const axios = require("axios");
+require("dotenv").config();
 
+const apiKey = process.env.EXCHANGE_RATE_API_KEY;
+const baseUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+async function convertCurrency(fromCurrency, toCurrency, amount) {
+  try {
+    const response = await axios.get(`${baseUrl}${fromCurrency}`);
+    const exchangeRates = response.data.conversion_rates;
+    const rate = exchangeRates[toCurrency];
+
+    if (!rate) {
+      throw new Error(
+        `Unable to get currency conversion rate for ${toCurrency}`
+      );
+    }
+    return rate;
+    // const convertedAmount = (amount * rate).toFixed(2);
+    // console.log(
+    //   `${amount} ${fromCurrency} is equal to ${convertedAmount} ${toCurrency}`
+    // );
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
 const createTour = async (req, res) => {
   console.log(req.body);
   const getCurrentDate = () => {
@@ -21,16 +46,17 @@ const createTour = async (req, res) => {
     return res.status(400).json({ msg: "invalid user  ...." });
   }
   try {
-    console.log("inside try");
+    const rate =await convertCurrency(req.body.currency, "INR");
+ 
     const voucherCreated = await Vouchers.create({
       statusType: "Created",
       tourLocation: req.body.city,
       userId: req.body.userId,
       currency: req.body.currency,
       tourDate: getCurrentDate(),
+      exchangeRates:rate
     });
 
-    console.log("voucher created......", voucherCreated);
     if (!voucherCreated) {
       return res
         .status(400)
