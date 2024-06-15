146ccd794d2eca91bfd97c04aecf68f78cebd568
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-06-15 10:22:30 +0530

    Added build

--- util/database.js
+++ util/database.js
@@ -2,9 +2,7 @@ const sequelize = require("sequelize");
 require("dotenv").config();
 const db = new sequelize("TourVoucher", process.env.SQLSCHEMANAME,  process.env.SQLSCHEMAPASSWORD, {
   dialect: "mysql",
-
   host:  process.env.SQLHOST,
-  
 });
 
 module.exports = db;
