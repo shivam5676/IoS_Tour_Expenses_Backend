1c971bc43f8a7c12bf878c1e9ad241c7906c04e8
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-08-02 14:25:45 +0530

    added new bulid file fixed some bugs

--- Controllers/user/addExpense.js
+++ Controllers/user/addExpense.js
@@ -2,8 +2,6 @@ const path = require("path");
 const voucherExpense = require("../../models/voucherExpense");
 const fs = require("fs");
 
-
-
 const addExpense = async (req, res, next) => {
   const {
     amount,
@@ -18,29 +16,32 @@ const addExpense = async (req, res, next) => {
   } = req.body;
 
   const uploadDir = path.join(__dirname, "..", "..", "uploads");
+
   if (!fs.existsSync(uploadDir)) {
     try {
-      fs.mkdirSync(uploadDir);
+      fs.mkdirSync(uploadDir, { recursive: true }); // Ensure the directory is created with recursive flag
     } catch (err) {
       console.error("Error creating upload directory:", err);
       return res.status(500).json({ msg: "Failed to create upload directory" });
     }
   }
+
   if (!amount) {
-    return res.status(400).json({ msg: "amount can not null" });
+    return res.status(400).json({ msg: "amount cannot be null" });
   }
   if (isNaN(amount)) {
-    return res.status(400).json({ msg: "amount can not string" });
+    return res.status(400).json({ msg: "amount must be a number" });
   }
   if (!expenseType) {
-    return res.status(400).json({ msg: "expense type can not null" });
+    return res.status(400).json({ msg: "expense type cannot be null" });
   }
   if (!paymentType) {
-    return res.status(400).json({ msg: "payment type can not null" });
+    return res.status(400).json({ msg: "payment type cannot be null" });
   }
   if (!date) {
-    return res.status(400).json({ msg: "please select date " });
+    return res.status(400).json({ msg: "please select date" });
   }
+
   if (billImage) {
     const matches = billImage.match(/^data:image\/(\w+);base64,/);
     if (!matches) {
@@ -49,11 +50,11 @@ const addExpense = async (req, res, next) => {
     const extension = matches[1];
     const base64Data = billImage.replace(/^data:image\/\w+;base64,/, "");
     const buffer = Buffer.from(base64Data, "base64");
-    const filename = `uploads/${Date.now()}-billImage.${extension}`;
+    const filename = path.join(uploadDir, `${Date.now()}-billImage.${extension}`);
 
     fs.writeFile(filename, buffer, async (err) => {
       if (err) {
-        console.error(err);
+        console.error("Error saving image:", err);
         return res.status(500).json({ msg: "Failed to save image" });
       }
       try {
@@ -68,46 +69,31 @@ const addExpense = async (req, res, next) => {
           userId,
           imagePath: filename,
         });
-        if (response) {
-          return res.status(200).json({ expenseData: response });
-        }
-       
-        return res
-          .status(400)
-          .json({ msg: "some problem while saving Your expense" });
+        return res.status(200).json({ expenseData: response });
       } catch (err) {
-        return res
-          .status(400)
-          .json({ msg: "something went wrong !! try again ...." });
+        console.error("Error saving expense:", err);
+        return res.status(400).json({ msg: "Something went wrong, try again" });
       }
     });
-  }else{
-
+  } else {
     try {
-    const response = await voucherExpense.create({
-      Amount: +amount,
-      expenseType,
-      voucherNo: voucher,
-      paymentType,
-      date,
-      description,
-      VoucherId: voucherId,
-      userId,
-      imagePath: null,
-    });
-    if (response) {
+      const response = await voucherExpense.create({
+        Amount: +amount,
+        expenseType,
+        voucherNo: voucher,
+        paymentType,
+        date,
+        description,
+        VoucherId: voucherId,
+        userId,
+        imagePath: null,
+      });
       return res.status(200).json({ expenseData: response });
+    } catch (err) {
+      console.error("Error saving expense:", err);
+      return res.status(400).json({ msg: "Something went wrong, try again" });
     }
-   
-    return res
-      .status(400)
-      .json({ msg: "some problem while saving Your expense" });
-  } catch (err) {
-    return res
-      .status(400)
-      .json({ msg: "something went wrong !! try again ...." });
   }
-  }
-  
 };
+
 module.exports = addExpense;
