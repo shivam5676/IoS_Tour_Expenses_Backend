d80f897fdc880e3f7e9e4cdc29707240462e8d68
shivam singh <ssultanpura9@gmail.com>
2024-08-04 22:55:03 +0530

    added and tesrted some functionality

--- util/database.js
+++ util/database.js
@@ -2,22 +2,22 @@ const sequelize = require("sequelize");
 require("dotenv").config();
 const fs = require('fs');
 const path = require('path');
-// const db = new sequelize("TourVoucher", process.env.SQLUSERNAME,  process.env.SQLSCHEMAPASSWORD, {
-//   dialect: "mysql",
-//   host:  process.env.SQLHOST,
-// });
-
-const caPath = path.resolve(__dirname, '../', 'ca.pem');
-
-const db = new sequelize("TourVoucher", process.env.SQLUSERNAME, process.env.SQLSCHEMAPASSWORD, {
+const db = new sequelize("TourVoucher", process.env.SQLUSERNAME,  process.env.SQLSCHEMAPASSWORD, {
   dialect: "mysql",
-  host: process.env.SQLHOST,
-  logging:false,
-  port: 19424, // Specify the custom port
-  dialectOptions: {
-    ssl: {
-      ca: fs.readFileSync(caPath)
-    }
-  }
+  host:  process.env.SQLHOST,
 });
+
+// const caPath = path.resolve(__dirname, '../', 'ca.pem');
+
+// const db = new sequelize("TourVoucher", process.env.SQLUSERNAME, process.env.SQLSCHEMAPASSWORD, {
+//   dialect: "mysql",
+//   host: process.env.SQLHOST,
+//   logging:false,
+//   port: 19424, // Specify the custom port
+//   dialectOptions: {
+//     ssl: {
+//       ca: fs.readFileSync(caPath)
+//     }
+//   }
+// });
 module.exports = db;
